1305db0eb34312cf1f5a49fc27f8f76d8ce812f3
kou0402 <kou.4869.biz@gmail.com>
2019-12-31 13:08:42 +0900

    style: add home page

--- src/main/angular/src/app/app-routing.module.ts
+++ src/main/angular/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 import { TodoComponent } from './pages/todo/todo.component';
 
 
 const routes: Routes = [
+  { path: 'home', component: HomeComponent },
   { path: 'todo', component: TodoComponent },
 ];
 
--- src/main/angular/src/app/app.module.ts
+++ src/main/angular/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './shered/nav-bar/nav-bar.component';
 import { TodoComponent } from './pages/todo/todo.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    TodoComponent
+    TodoComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
