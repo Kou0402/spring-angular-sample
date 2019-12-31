ede940da24c42bcaea660b1702fd6cd3bd082914
kou0402 <kou.4869.biz@gmail.com>
2019-12-31 11:52:54 +0900

    style: create pages

--- src/main/angular/src/app/app.module.ts
+++ src/main/angular/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './shered/nav-bar/nav-bar.component';
+import { TodoComponent } from './pages/todo/todo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    TodoComponent
   ],
   imports: [
     BrowserModule,
