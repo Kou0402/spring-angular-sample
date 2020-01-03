347f659932b7e8f88e3fa9d3fe77217217fcf198
kou0402 <kou.4869.biz@gmail.com>
2020-01-03 19:43:32 +0900

    chore: chenge directory name

--- src/main/java/com/todo/app/HelloWorld.java
+++ src/main/java/com/todo/app/HelloWorld.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.todo.app;
 
 import java.util.List;
 import java.util.Map;
--- src/main/java/com/todo/app/Lecture10Controller.java
+++ src/main/java/com/todo/app/Lecture10Controller.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.todo.app;
 
 import java.util.Date;
 import java.util.List;
